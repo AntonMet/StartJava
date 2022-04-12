59d6de0782dbc05e2288b8956be8a59364894b0a
AntonMet <antonmetelev93@gmail.com>
2022-04-12 22:34:32 +0300

    дополнено

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -5,7 +5,7 @@ class CyclesTheme {
         int a;
         int even = 0;
         int odd = 0;
-        for(a = -10; a < 22; a++) {
+        for (a = -10; a < 22; a++) {
             if (a % 2 == 0) {
                 even = even + a;
             } else {
@@ -37,7 +37,7 @@ class CyclesTheme {
         }
         System.out.println("Числа между минимальным и максимальным:");
         int i;
-        for(i = min + 1; i < max; i++) {
+        for (i = min + 1; i < max; i++) {
             System.out.println(i);
         }
         System.out.println();
@@ -63,15 +63,16 @@ class CyclesTheme {
             System.out.printf("%4d", a);
             b++;
             if (b % 5 == 0) {
-                System.out.println();
+                System.out.println("");
             }
         }
-        for(; b % 5 != 0; b++) {
+        for (; b % 5 != 0; b++) {
             System.out.printf("%4d", 0);
         }
-        System.out.println();
+        System.out.println("");
 
         //Проверка количества единиц на четность
+        System.out.println("Проверка количества единиц на четность");
         a = 3141591;
         b = 0;
         while (a > 9) {
@@ -80,18 +81,19 @@ class CyclesTheme {
             }
             a = a / 10;
         }
-        if(b % 2 == 0) {
-            System.out.println ("Количество единиц четное: " + b);
+        if (b % 2 == 0) {
+            System.out.println ("Количество единиц четное: " + b + "\n");
         } else {
-            System.out.println ("Количество единиц нечетное: " + b);
+            System.out.println ("Количество единиц нечетное: " + b + "\n");
         }
 
         //Отображение фигур в консоли
-        for(a = 0; a < 5; a++) {
-            for(b = 0; b < 10; b++) {
+        System.out.println("Отображение фигур в консоли");
+        for (a = 0; a < 5; a++) {
+            for (b = 0; b < 10; b++) {
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println("");
         }
         a = 0;
         b = 0;
@@ -101,7 +103,7 @@ class CyclesTheme {
                 a++;
                 System.out.print("#");
             }
-            System.out.println();
+            System.out.println("");
             a = 0;
             b++;
             c--;
@@ -117,7 +119,7 @@ class CyclesTheme {
             a = 0;
             b++;
             c++;
-            System.out.println();
+            System.out.println("");
         } while (b < 3);
         a = 0;
         b = 0;
@@ -130,18 +132,67 @@ class CyclesTheme {
             a = 0;
             b++;
             c--;
-            System.out.println();
+            System.out.println("");
         } while (b < 2);
-        System.out.println();
+        System.out.println("");
 
         //Отображение ASCII-символов
-        for(a = 0; a < 128; a++) {
+        System.out.println("Отображение ASCII-символов");
+        for (a = 0; a < 128; a++) {
             if (a % 2 != 0) {
                 System.out.println((char) a);
             }
         }
+        System.out.println("");
 
         //Проверка, является ли число палиндромом
+        System.out.println("Проверка, является ли число палиндромом");
         a = 12321;
+        if (a / 10000 == a % 10) {
+            if ((a / 1000) % 10 == (a % 100) / 10) {
+                System.out.println("число палиндром" + "\n");
+            } else {
+                System.out.println("число не палиндром" + "\n");
+            }
+        } else {
+            System.out.println("число не палиндром" + "\n");
+        }
+
+        //Определение, является ли число счастливым
+        System.out.println("Определение, является ли число счастливым");
+        a = 325891;
+        b = a / 1000;
+        c = a % 1000;
+        int firstSum = 0;
+        int secondSum = 0;
+        for (i = 0; i < 3; i++) {
+            firstSum = firstSum + b % 10;
+            b = b / 10;
+        }
+        for (i = 0; i < 3; i++) {
+            secondSum = secondSum + c % 10;
+            c = c / 10;
+        }
+        System.out.println(a / 1000 + " = " + firstSum);
+        System.out.println(a % 1000 + " = " + secondSum + "\n");
+
+        //Вывод таблицы умножения Пифагора
+        System.out.println("Вывод таблицы умножения Пифагора");
+        System.out.printf("%5s", "|");
+        for (i = 2; i < 10; i++) {
+            System.out.printf("%4d", i);
+        }
+        System.out.println();
+        for (i = 1; i < 10; i++) {
+            System.out.printf("%4s", "____");
+        }
+        System.out.println("");
+        for (i = 2; i < 10; i++) {
+            System.out.printf(i + "%4s", "|");
+            for (int j = 2; j < 10; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println("");
+        }
     }
 }
