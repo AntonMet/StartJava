fac6b8fcb61e60ee82b4382a0dcdc757a2223aed
AntonMet <antonmetelev93@gmail.com>
2022-04-18 22:24:49 +0300

    исправил

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -38,39 +38,38 @@ class CyclesTheme {
         for (int i = max - 1; i > min; i--) {
             System.out.println(i);
         }
-        System.out.println("\n3. Вывод реверсивного числа и суммы его цифр");
 
         //Вывод реверсивного числа и суммы его цифр
-        a = 1234;
+        System.out.println("\n3. Вывод реверсивного числа и суммы его цифр");
+        int scrNum = 1234;
         int sum = 0;
         do {
-            b = a % 10;
-            a = a / 10;
-            sum += b;
-            System.out.print(b);
-        } while (a > 9);
-        sum += a;
-        System.out.println(a + "\n" + "Сумма цифр: " + sum + "\n");
+            int digit = scrNum % 10;
+            scrNum /= 10;
+            sum += digit;
+            System.out.print(digit);
+        } while (scrNum / 10 != 0);
+        sum += scrNum;
+        System.out.println(scrNum + "\n" + "Сумма цифр: " + sum + "\n");
 
         //Вывод чисел на консоль в несколько строк
         System.out.println("4. Вывод чисел на консоль в несколько строк");
-        a = 1;
-        b = 0;
-        for (a = 1; a <= 24; a += 2) {
-            System.out.printf("%4d", a);
-            b++;
-            if (b % 5 == 0) {
+        int counter = 0;
+        for (int i = 1; i <= 24; i += 2) {
+            System.out.printf("%4d", i);
+            counter++;
+            if (counter % 5 == 0) {
                 System.out.print("\n");
             }
         }
-        for (; b % 5 != 0; b++) {
+        for (; counter % 5 != 0; counter++) {
             System.out.printf("%4d", 0);
         }
-        System.out.println("\n5. Проверка количества единиц на четность");
 
         //Проверка количества единиц на четность
-        int scrNum = 3141591;
-        int counter = 0;
+        System.out.println("\n5. Проверка количества единиц на четность");
+        scrNum = 3141591;
+        counter = 0;
         while (scrNum % 10 != 0) {
             if (scrNum % 10 == 1) {
                 counter++;
@@ -85,27 +84,27 @@ class CyclesTheme {
 
         //Отображение фигур в консоли
         System.out.println("6. Отображение фигур в консоли");
-        for (a = 0; a < 5; a++) {
-            for (b = 0; b < 10; b++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
                 System.out.print("*");
             }
             System.out.print("\n");
         }
         a = 0;
-        b = 0;
+        counter = 0;
         c = 5;
-        while (b < 5) {
+        while (counter < 5) {
             while (a < c) {
                 a++;
                 System.out.print("#");
             }
             System.out.print("\n");
             a = 0;
-            b++;
+            counter++;
             c--;
         }
         a = 0;
-        b = 0;
+        counter = 0;
         c = 1;
         do {
             do {
@@ -113,12 +112,12 @@ class CyclesTheme {
                 a++;
             } while (a < c);
             a = 0;
-            b++;
+            counter++;
             c++;
             System.out.print("\n");
-        } while (b < 3);
+        } while (counter < 3);
         a = 0;
-        b = 0;
+        counter = 0;
         c = 2;
         do {
             do {
@@ -126,39 +125,35 @@ class CyclesTheme {
                 a++;
             } while (a < c);
             a = 0;
-            b++;
+            counter++;
             c--;
             System.out.print("\n");
-        } while (b < 2);
-        System.out.println("\n7. Отображение ASCII-символов");
+        } while (counter < 2);
 
         //Отображение ASCII-символов
-        System.out.println("Dec  Char");
-        for (a = 0; a < 128; a++) {
-            if (a % 2 != 0) {
-                System.out.printf("%5d%5s", a, (char) a + "\n");
-                if (a == 27) {
+        System.out.println("\n7. Отображение ASCII-символов");
+        System.out.println("Dec Char");
+        for (char i = 0; i < 128; i++) {
+            if (i % 2 != 0) {
+                System.out.printf("%3d%3s", (int) i, i + "\n");
+                if (i == 27) {
                     System.out.print("\n");
                 }
             }
         }
-        System.out.println("\n8. Проверка, является ли число палиндромом");
 
         //Проверка, является ли число палиндромом
+        System.out.println("\n8. Проверка, является ли число палиндромом");
         scrNum = 12321;
         a = 10000;
         boolean palindrome = true;
         for (int i = 0; i < 2; i++) {
             if (scrNum / a != scrNum % 10) {
                 palindrome = false;
-                scrNum %= a;
-                scrNum /= 10;
-                a /= 100;
-            } else {
-                scrNum %= a;
-                scrNum /= 10;
-                a /= 100;
             }
+            scrNum %= a;
+            scrNum /= 10;
+            a /= 100;
         }
         if (palindrome) {
             System.out.println("Число палиндром" + "\n");
@@ -168,21 +163,19 @@ class CyclesTheme {
 
         //Определение, является ли число счастливым
         System.out.println("9. Определение, является ли число счастливым");
-        int number = 325891;
-        int firstHalfNumber = number / 1000;
-        int secondHalfNumber = number % 1000;
+        scrNum = 325891;
+        int firstHalfNumber = scrNum / 1000;
+        int secondHalfNumber = scrNum % 1000;
         int firstSum = 0;
         int secondSum = 0;
         for (int i = 0; i < 3; i++) {
             firstSum += firstHalfNumber % 10;
             firstHalfNumber /= 10;
-        }
-        for (int i = 0; i < 3; i++) {
             secondSum += secondHalfNumber % 10;
             secondHalfNumber /= 10;
         }
-        System.out.println(number / 1000 + " = " + firstSum);
-        System.out.println(number % 1000 + " = " + secondSum);
+        System.out.println(scrNum / 1000 + " = " + firstSum);
+        System.out.println(scrNum % 1000 + " = " + secondSum);
         if (firstSum == secondSum) {
             System.out.println("Число счастливое" + "\n");
         } else {
