d27b1c98d4ad54ea00f25a40941383e0fa0ceaf0
AntonMet <antonmetelev93@gmail.com>
2022-04-20 22:14:31 +0300

    добавлен

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+class Calculator {
+    public static void main(String[] args) {
+        int firstNumber = 3;
+        int secondNumber = 6;
+        char sign = '^';
+        int result = 0;
+        if (sign == '+') {
+            result = firstNumber + secondNumber;
+        } else if (sign == '-') {
+            result = firstNumber - secondNumber;
+        } else if (sign == '*') {
+            result = firstNumber * secondNumber;
+        } else if (sign == '/') {
+            result = firstNumber / secondNumber;
+        } else if (sign == '^') {
+            result = firstNumber;
+            for (int i = 0; i < secondNumber - 1; i++) {
+                result *= firstNumber;
+            }
+        } else if (sign == '%') {
+            result = firstNumber % secondNumber;
+        }
+        System.out.println(firstNumber + " " + sign + " " + secondNumber + " = " + result);
+    }
+}
