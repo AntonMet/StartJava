a93c98363c72ac73ae9277701ba6f484d5a8c26b
AntonMet <antonmetelev93@gmail.com>
2022-04-25 22:33:45 +0300

    добавлено

---
+++ Lesson_2/Person.java
@@ -0,0 +1,21 @@
+class Person {
+    String sex = "male";
+    String name = "Ignat";
+    double height = 1.8;
+    int weight = 75;
+    int age = 25;
+    void walk() {
+    }
+
+    void sit() {
+    }
+
+    void run() {
+    }
+
+    void speak() {
+    }
+
+    void learnJava() {
+    }
+}
